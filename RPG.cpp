3de5922ac362f71618c3f369dd1a644749884dc6
HO6152 <salehkanoun0@gmail.com>
2025-09-15 12:47:00 +0200

    Add files via upload

---
+++ RPG.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+
+int struct Character {
+    std::string name;
+    int health;
+    int attack;
+    int defense;
+
+    Character(std::string n, int h, int a, int d) : name(n), health(h), attack(a), defense(d) {}
+
+    std::string name = "Saleh";
+    int healt = 100;
+    int attack = 20;
+    int defense = 10;
+
+    }
+    
+    int struct Enemyf {
+        std::string name;
+        int health;
+        int attack;
+        int defense;
+        Enemy(std::string n, int h, int a, int d) : name(n), health(h), attack(a), defense(d) {}
+
+        std:: string name = "Benjamin";
+        int health = 150;
+        int attack = 15;
+        int defense = 5;
+    
+    }
